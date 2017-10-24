a998a464ed126637d1ce94651505653157f5bf8a
luchoguti <luchoguti9@gmail.com>
2017-10-24 10:46:50 -0500

    Update conexion_bd.php

--- bd/conexion_bd.php
+++ bd/conexion_bd.php
@@ -9,15 +9,15 @@ $nombre_bd = "bodegayc_sistema";
 
 //Server produccion
 //$nombre_servidor = "localhost";
-//$nombre_usuario = "bodegayc_sistema";
-//$contrasena = "s1st3m4";
-//$nombre_bd = "bodegayc_sistema";
+//$nombre_usuario = "";
+//$contrasena = "";
+//$nombre_bd = "";
 
 //Server PruebaVentas
 //$nombre_servidor = "localhost";
-//$nombre_usuario = "bodegayc_sistema";
-//$contrasena = "s1st3m4";
-//$nombre_bd = "bodegayc_pruebaventas";
+//$nombre_usuario = "";
+//$contrasena = "";
+//$nombre_bd = "";
 
 $conexion = mysql_connect($nombre_servidor,$nombre_usuario,$contrasena) or die ('Error en la conexion: ' . mysql_error());
 mysql_select_db($nombre_bd) or die ('Error en la conexion: ' . mysql_error());
